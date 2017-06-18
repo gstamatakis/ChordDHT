d2d66a761ac085e4e67843c2a30a38d0dced782a
gstamatakis <giorgoshstam@gmail.com>
2017-06-18 16:29:56 +0300

    Init

--- src/main/java/BootStrapNode.java
+++ src/main/java/BootStrapNode.java
@@ -1,18 +1,28 @@
-import java.rmi.*;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 //Interface for the BootStrap Server node
-public interface BootStrapNode extends Remote{
-	
-	public ArrayList<NodeInfo> addNodeToRing(String ipaddress, String port, int zoneID) throws RemoteException;
-	public void removeNodeFromRing(NodeInfo n) throws RemoteException;
-	public void acknowledgeNodeJoin(int nodeID) throws RemoteException;
-	public void displayNodesInRing() throws RemoteException;
-	public int getNodesInRing() throws RemoteException;
-	public ArrayList<Integer> getNodeIds() throws RemoteException;
-	public int getMaxNodesInRing() throws RemoteException;
-	public int generate_ID(String key, int maxNodes) throws RemoteException, NoSuchAlgorithmException;
-	public boolean isZoneFilled(int zoneID) throws RemoteException;
-	public NodeInfo findNewSuccessor(NodeInfo n, NodeInfo dead_node) throws RemoteException;
+public interface BootStrapNode extends Remote {
+
+    ArrayList<NodeInfo> addNodeToRing(String ipaddress, String port, int zoneID) throws RemoteException;
+
+    void removeNodeFromRing(NodeInfo n) throws RemoteException;
+
+    void acknowledgeNodeJoin(int nodeID) throws RemoteException;
+
+    void displayNodesInRing() throws RemoteException;
+
+    int getNodesInRing() throws RemoteException;
+
+    ArrayList<Integer> getNodeIds() throws RemoteException;
+
+    int getMaxNodesInRing() throws RemoteException;
+
+    int generate_ID(String key, int maxNodes) throws RemoteException, NoSuchAlgorithmException;
+
+    boolean isZoneFilled(int zoneID) throws RemoteException;
+
+    NodeInfo findNewSuccessor(NodeInfo n, NodeInfo dead_node) throws RemoteException;
 }
