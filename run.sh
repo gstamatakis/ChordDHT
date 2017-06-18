4b2de1af07337307186b6d42f62ea776d3da896f
gstamatakis <giorgoshstam@gmail.com>
2017-06-18 22:14:31 +0300

    Working again.

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+###BootStrap Node
 java -Djava.rmi.server.useCodebaseOnly=false -Djava.security.policy=security.policy -Djava.rmi.server.codebase=file:/home/gstamatakis/Projects/ChordDHT/target/classes/ -jar JARs/BootStrapNode.jar
+
+###Client/Chord Node
 java -Djava.rmi.server.useCodebaseOnly=false -Djava.security.policy=security.policy -Djava.rmi.server.codebase=file:/home/gstamatakis/Projects/ChordDHT/target/classes/ -jar JARs/ChordNode.jar localhost 147.27.70.106 5
