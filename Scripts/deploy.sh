415283ffa368c472e0a475b42d7944733c43c87c
gstamatakis <giorgoshstam@gmail.com>
2017-06-26 11:31:45 +0300

    Docker testing.

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -2,8 +2,7 @@
 for (( i=1100; i<1150; i++ ))
 do
 	echo "Container $i is being created."
-	docker run -p $i:1099 --privileged dd rmiregistry & java -jar ChordDHT-1.0-SNAPSHOT-jar-with-dependencies.jar localhost 137.27.70.106 32 &
+	#docker run -p $i:1099 --privileged dd rmiregistry & java -jar target/ChordDHT-1.0-SNAPSHOT-jar-with-dependencies.jar localhost 137.27.70.106 32 &
+	docker run -p $i:1099 --privileged dd & java -jar target/ChordDHT-1.0-SNAPSHOT-jar-with-dependencies.jar localhost 137.27.70.106 32 &
 	sleep 0.5
 done
-
-#-Djava.rmi.server.useCodebaseOnly=false -Djava.security.policy=client.policy -Djava.rmi.server.codebase=file:/app/target/classes/
