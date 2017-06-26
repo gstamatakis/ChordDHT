45ffad461e835a6a31b9b1ed318e5c3318640722
gstamatakis <giorgoshstam@gmail.com>
2017-06-26 22:06:00 +0300

    Again

--- src/main/java/BootStrapNodeImpl.java
+++ src/main/java/BootStrapNodeImpl.java
@@ -43,7 +43,7 @@ public class BootStrapNodeImpl extends UnicastRemoteObject implements BootStrapN
     public static void main(String[] args) throws Exception {
         try {
             BootStrapNodeImpl bnode = new BootStrapNodeImpl();
-            System.setProperty("java.rmi.server.hostname","127.0.0.1");
+            System.setProperty("java.rmi.server.hostname","localhost");
             Naming.rebind("ChordRing", bnode);
             noOfNodes = 0;
             System.out.println("Waiting for nodes to join or leave the Chord Ring");
